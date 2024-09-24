ae2a89892f173a3ad1ccb50b1051249cb2dc982f
Arsen-Koshparenko <arsen.koshparenko@mths.ca>
2024-09-24 11:00:54 -0400

    Temperature in kelvin

--- main.ts
+++ main.ts
@@ -1,8 +1,18 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Arsen
+ * Created on: Sep 2024
+ * This program measures the temperature of the room in kelvin
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+let temp = 0
+
+input.onButtonPressed(Button.A, function () {
+    temp = input.temperature()
+    temp = (temp + 273.15)
+    temp = Math.round(temp)
+    basic.clearScreen()
+    basic.showString('The Temperature is: ' + temp.toString() + 'kelvin.')
+})
